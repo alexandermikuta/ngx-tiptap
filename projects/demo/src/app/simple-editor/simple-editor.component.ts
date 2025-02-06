b9afdc97f34985c08f3d1ff6856a47ee1c501388
Alexander Mikuta <alexander.mikuta@imbus.de>
2025-02-06 15:33:51 +0100

    added resize and scrolling to editor-container

--- projects/demo/src/app/simple-editor/simple-editor.component.ts
+++ projects/demo/src/app/simple-editor/simple-editor.component.ts
@@ -6,6 +6,7 @@ import { Content, Editor } from '@tiptap/core';
 import StarterKit from '@tiptap/starter-kit';
 import Placeholder from '@tiptap/extension-placeholder';
 import TextStyle from '@tiptap/extension-text-style';
+import Image from '@tiptap/extension-image'
 import { Color }from '@tiptap/extension-color';
 import { TiptapEditorDirective } from 'ngx-tiptap';
 
@@ -18,21 +19,31 @@ import { TiptapEditorDirective } from 'ngx-tiptap';
 export class SimpleEditorComponent implements OnDestroy {
   value: Content = `Beispiel Rich-Text...`;
 
+
   editor = new Editor({
     extensions: [
       StarterKit,
       Placeholder,
       TextStyle,
-      Color
+      Color,
+      Image
 
     ],
     editorProps: {
       attributes: {
-        class: 'p-2 border-black border-2 outline-none h-96',
+        class: 'p-2 border-black border-2 outline-none h-96 overflow-auto resize-y',
         spellCheck: 'false',
       },
     },
   });
+
+  public addImage = () => {
+    const url = window.prompt('URL')
+
+    if (url) {
+      this.editor.chain().focus().setImage({ src: url }).run()
+    }
+  };
 
   handleValueChange(value: Content): void {
     this.value = value;
