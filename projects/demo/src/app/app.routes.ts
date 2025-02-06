39d9d50dba19cb0a7a529ff0bf551da596b8b406
Alexander Mikuta <alexander.mikuta@imbus.de>
2025-02-06 08:52:11 +0100

    basic tiptap-setup

--- projects/demo/src/app/app.routes.ts
+++ projects/demo/src/app/app.routes.ts
@@ -6,24 +6,4 @@ export const routes: Routes = [
     loadComponent: () => import('./simple-editor/simple-editor.component').then((m) => m.SimpleEditorComponent),
     title: 'Simple Editor',
   },
-  {
-    path: 'editor-reactive',
-    loadComponent: () => import('./simple-editor-reactive/simple-editor-reactive.component').then((m) => m.SimpleEditorReactiveComponent),
-    title: 'Reactive Forms',
-  },
-  {
-    path: 'floating-menu',
-    loadComponent: () => import('./floating-menu/floating-menu.component').then((m) => m.FloatingMenuComponent),
-    title: 'Floating Menu',
-  },
-  {
-    path: 'bubble-menu',
-    loadComponent: () => import('./bubble-menu/bubble-menu.component').then((m) => m.BubbleMenuComponent),
-    title: 'Bubble Menu',
-  },
-  {
-    path: 'nodeview-renderer',
-    loadComponent: () => import('./nodeview-renderer/nodeview-renderer.component').then((m) => m.NodeviewRendererComponent),
-    title: 'Interactive NodeViews',
-  },
 ];
