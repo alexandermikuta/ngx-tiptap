445029deabe3cfc1dea5237622e654da5860ff79
Alexander Mikuta <alexander.mikuta@imbus.de>
2025-02-06 14:36:32 +0100

    added more functionality and html output

--- projects/demo/src/app/simple-editor/simple-editor.component.ts
+++ projects/demo/src/app/simple-editor/simple-editor.component.ts
@@ -14,13 +14,7 @@ import { TiptapEditorDirective } from 'ngx-tiptap';
   styleUrls: ['./simple-editor.component.css'],
 })
 export class SimpleEditorComponent implements OnDestroy {
-  value: Content = `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-    Lorem Ipsum has been the <strong>industry's standard dummy text ever since the 1500s</strong>,
-    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-    It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-    and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
-    `;
+  value: Content = `Beispiel Rich-Text...`;
 
   editor = new Editor({
     extensions: [
@@ -29,7 +23,7 @@ export class SimpleEditorComponent implements OnDestroy {
     ],
     editorProps: {
       attributes: {
-        class: 'p-2 border-black border-2 rounded-b-md outline-none',
+        class: 'p-2 border-black border-2 outline-none h-96',
         spellCheck: 'false',
       },
     },
